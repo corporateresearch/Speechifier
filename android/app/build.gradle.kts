b908fe00bb852f680e2eb2d2cf1bca8d868f1225
linuxmasterrace <linuxmasterrace@users.noreply.github.com>
2026-06-03 06:31:04 +0300

    fix: opt-in to ExperimentalLayoutApi at module level to fix CI build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,6 +49,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.foundation.layout.ExperimentalLayoutApi",
+        )
     }
     buildFeatures {
         compose = true
